db8f31d035f38c73eb5804725637895f6e9bb781
Adam Zolyak <adam@tinkurlab.com>
2016-02-10 12:45:08 -0700

    Adding to dos and removing unused examples

--- scripts/chatwithme.js
+++ scripts/chatwithme.js
@@ -19,11 +19,21 @@ module.exports = function (robot) {
     robot.respond(/there is an outage/, function (msg) {
         var dialog = switchBoard.startDialog(msg);
 
+        //TODO: Ask what is impacted to send when everyone is notified
+
+        //TODO: Change response case to lower to match YES, Yes, yes
+
         msg.reply('Oh no!  Should I notify everyone? (yes|no)');
         dialog.addChoice(/yes/i, function (msg2) {
+            
+            //TODO: Add notification to flows here
+
             msg2.reply('Ok, I told them what\'s up!  Should I sound the alarm? (yes|no)');
             dialog.addChoice(/yes/, function (msg3) {
                 msg3.reply('Alarm activated!');
+                
+                //TODO: Add call to TinkurRadiator Node Service here
+
             });
             dialog.addChoice(/no/, function (msg3) {
                 msg3.reply('Ok, but I think it was a good idea.');
@@ -32,30 +42,6 @@ module.exports = function (robot) {
         });
         dialog.addChoice(/no/i, function (msg2) {
             msg.reply('Whew!  You scared me...');
-        });
-    });
-
-    robot.respond(/jump/, function (msg) {
-        var dialog = switchBoard.startDialog(msg);
-        msg.reply('Sure, How many times?');
-        
-        dialog.addChoice(/([0-9]+)/i, function (msg2) {
-            var times = parseInt(msg2.match[1], 10);
-            for (var i = 0; i < times; i++) {
-                msg.emote("Jumps"); //We can use the original message too.
-            }
-        });
-    });
-    
-    
-  robot.respond(/.*the mission/, function (msg) {
-        msg.reply('Your have 5 seconds to accept your mission, or this message will self-destruct');
-        var dialog = switchBoard.startDialog(msg, 5000); //5 Second timeout
-        dialog.timeout = function (msg2) {
-            msg2.emote('Boom');
-        }
-        dialog.addChoice(/yes/i, function (msg2) {
-            msg2.reply('Great! Here are the details...');
         });
     });
 };
